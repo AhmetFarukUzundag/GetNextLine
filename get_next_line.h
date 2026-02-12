8f90138e486627156a9598c7b57b8a5e0c10d2a0
Ahmet Uzundağ <ahmetrabet@gmail.com>
2026-02-12 17:24:21 +0300

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: auzundag <auzundag@student.42istanbul.com.tr  + +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/12 16:26:33 by auzundag          #+#    #+#             */
+/*   Updated: 2026/02/12 16:29:53 by auzundag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#endif
